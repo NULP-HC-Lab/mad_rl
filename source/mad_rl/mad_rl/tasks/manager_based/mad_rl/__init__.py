859f61f045b9e17c8f7d61d383e826dfd72a39e0
Andrii Osidach <andriiosidach@gmail.com>
2026-01-10 15:32:46 +0200

    feat: add rough training terrain configs

--- source/mad_rl/mad_rl/tasks/manager_based/mad_rl/__init__.py
+++ source/mad_rl/mad_rl/tasks/manager_based/mad_rl/__init__.py
@@ -19,3 +19,23 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{__name__}.rsl_rl_ppo_cfg:RslRlPpoCfg",
     },
 )
+
+gym.register(
+    id="Mad-Rl-Rough-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.env_cfg:RoughEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{__name__}.rsl_rl_ppo_cfg:RslRlPpoCfg",
+    },
+)
+
+gym.register(
+    id="Mad-Rl-Rough-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.env_cfg:RoughEnvCfg_PLAY",
+        "rsl_rl_cfg_entry_point": f"{__name__}.rsl_rl_ppo_cfg:RslRlPpoCfg",
+    },
+)

3b7eaf79490f65e83ed2206c6cf1b095e3648b07
Andrii Osidach <andriiosidach@gmail.com>
2026-02-21 15:13:13 +0200

    feat: encourage exploration by pushing via velocity setting

--- source/mad_rl/mad_rl/tasks/manager_based/mad_rl/events_cfg.py
+++ source/mad_rl/mad_rl/tasks/manager_based/mad_rl/events_cfg.py
@@ -26,7 +26,7 @@ class EventsCfg:
         params={
             "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-3.14, 3.14)},
             "velocity_range": {
-                "x": (0.0, 0.0),
+                "x": (0.0, 1.0),
                 "y": (0.0, 0.0),
                 "z": (0.0, 0.0),
                 "roll": (0.0, 0.0),
@@ -50,4 +50,15 @@ class EventsCfg:
     apply_default_joint_position_term = EventTerm(
         func=apply_default_joint_position,
         mode="startup",
+    )
+
+    push_by_setting_velocity = EventTerm(
+        func=mdp.push_by_setting_velocity,
+        mode="interval",
+        interval_range_s=(0.0, 5.0),
+        params={
+            "velocity_range": {
+                "x": (0.0, 1.0),
+            }
+        },
     )
